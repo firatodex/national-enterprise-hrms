3ddd1e1291a733bc2d2adc750c5c9194bd54109f
OpsCraft <Afzal@afzaldosani.in>
2026-04-25 21:11:16 +0530

    Delete backend/src/src directory

--- backend/src/src/utils/db.js
+++
@@ -1,9 +0,0 @@
-const Database = require('better-sqlite3');
-const path = require('path');
-
-const DB_PATH = path.join(__dirname, '..', '..', 'db', 'hrms.db');
-const db = new Database(DB_PATH);
-db.pragma('journal_mode = WAL');
-db.pragma('foreign_keys = ON');
-
-module.exports = db;
